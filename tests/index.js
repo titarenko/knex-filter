5562adafed9208b3c93166fa1d17c24b103d93b6
Constantin Titarenko <constantin.titarenko@gmail.com>
2019-05-26 23:21:02 +0300

    replace mocha with tape since latter one is compatible with old node runtime

--- tests/index.js
+++ tests/index.js
@@ -1,69 +1,83 @@
-var should = require('should');
+var test = require('tape');
 var knex = require('knex')({ dialect: 'pg' });
 var filter = require('../').filter;
 
-describe('knex-filter', function () {
+test('should build empty query', function (t) {
+	t.plan(1)
+	var actual = knex('tab')
+		.where(filter({}))
+		.toSQL()
+		.sql;
+	var expected = 'select * from "tab"';
+	t.equal(actual, expected)
+});
 
-	describe('filter', function () {
-		it('should build empty query', function () {
-			knex('tab')
-				.where(filter({}))
-				.toSQL()
-				.sql
-				.should.eql('select * from "tab"');
-		});
-		it('should build simple query', function () {
-			knex('mytab')
-				.where(filter({ a: 'b', 'c >': 12, 'd in': [1, 2, 3], e: null }))
-				.toSQL()
-				.sql
-				.should.eql('select * from "mytab" where ("a" = ? and "c" > ? and "d" in (?, ?, ?) and "e" is null)');
-		});
-		it('should build query with "not"', function () {
-			knex('mytab')
-				.where(filter({ not: { a: null } }))
-				.toSQL()
-				.sql
-				.should.eql('select * from "mytab" where (not ("a" is null))');
-		});
-		it('should build query with "!"', function () {
-			knex('mytab')
-				.where(filter({ '!': { a: null } }))
-				.toSQL()
-				.sql
-				.should.eql('select * from "mytab" where (not ("a" is null))');
-		});
-		it('should build query with "and"', function () {
-			knex('mytab')
-				.where(filter({ and: { a: null, b: 12 } }))
-				.toSQL()
-				.sql
-				.should.eql('select * from "mytab" where ("a" is null and "b" = ?)');
-		});
-		it('should build query with "or"', function () {
-			knex('mytab')
-				.where(filter({ or: { a: null, b: 12 } }))
-				.toSQL()
-				.sql
-				.should.eql('select * from "mytab" where ((("a" is null) or ("b" = ?)))');
-		});
-		it('should build query with "and" and "or"', function () {
-			knex('mytab')
-				.where(filter({
-					a: 'b',
-					'c >': 12,
-					'd in': [1, 2, 3],
-					e: null,
-					not: { f: null },
-					or: {
-						k: 1,
-						'm like': '%Bob%'
-					}
-				}))
-				.toSQL()
-				.sql
-				.should.eql('select * from "mytab" where ("a" = ? and "c" > ? and "d" in (?, ?, ?) and "e" is null and not ("f" is null) and (("k" = ?) or ("m" like ?)))');
-		});
-	});
+test('should build simple query', function (t) {
+	t.plan(1)
+	var actual = knex('mytab')
+		.where(filter({ a: 'b', 'c >': 12, 'd in': [1, 2, 3], e: null }))
+		.toSQL()
+		.sql;
+	var expected = 'select * from "mytab" where ("a" = ? and "c" > ? and "d" in (?, ?, ?) and "e" is null)';
+	t.equal(actual, expected)
+});
 
+test('should build query with "not"', function (t) {
+	t.plan(1)
+	var actual = knex('mytab')
+		.where(filter({ not: { a: null } }))
+		.toSQL()
+		.sql;
+	var expected = 'select * from "mytab" where (not ("a" is null))';
+	t.equal(actual, expected)
+});
+
+test('should build query with "!"', function (t) {
+	t.plan(1)
+	var actual = knex('mytab')
+		.where(filter({ '!': { a: null } }))
+		.toSQL()
+		.sql;
+	var expected = 'select * from "mytab" where (not ("a" is null))';
+	t.equal(actual, expected)
+});
+
+test('should build query with "and"', function (t) {
+	t.plan(1)
+	var actual = knex('mytab')
+		.where(filter({ and: { a: null, b: 12 } }))
+		.toSQL()
+		.sql;
+	var expected = 'select * from "mytab" where ("a" is null and "b" = ?)';
+	t.equal(actual, expected)
+});
+
+test('should build query with "or"', function (t) {
+	t.plan(1)
+	var actual = knex('mytab')
+		.where(filter({ or: { a: null, b: 12 } }))
+		.toSQL()
+		.sql;
+	var expected = 'select * from "mytab" where ((("a" is null) or ("b" = ?)))';
+	t.equal(actual, expected)
+});
+
+test('should build query with "and" and "or"', function (t) {
+	t.plan(1)
+	var actual = knex('mytab')
+		.where(filter({
+			a: 'b',
+			'c >': 12,
+			'd in': [1, 2, 3],
+			e: null,
+			not: { f: null },
+			or: {
+				k: 1,
+				'm like': '%Bob%'
+			}
+		}))
+		.toSQL()
+		.sql
+	var expected = 'select * from "mytab" where ("a" = ? and "c" > ? and "d" in (?, ?, ?) and "e" is null and not ("f" is null) and (("k" = ?) or ("m" like ?)))';
+	t.equal(actual, expected)
 });
